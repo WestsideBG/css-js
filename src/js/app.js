e3f3499c4c3360c514d7e3bcac5d3943e7362c42
Samir Azzam <40363140+WestsideBG@users.noreply.github.com>
2022-11-29 21:38:04 +0200

    CECO E PEDAL

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,17 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
 });
+
+let addArticle = () => {
+  const articleTag = document.createElement("article");
+  articleTag.classList.add("message");
+  articleTag.innerHTML = "LOREM IPUSUM";
+  document.body.appendChild(articleTag);
+}
+
+document.body.addEventListener("click", (event) => {
+    for (let i = 0; i < 5; i++) {
+      addArticle();
+    }
+    event.preventDefault();
+})
